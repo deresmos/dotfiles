20eb225fbdf6ffaa2b30b17695da1b0f66baa19d
deresmos <deresmos@gmail.com>
2024-11-14 19:44:05 +0900

    Gardening

--- xdg_config_generator.sh
+++ xdg_config_generator.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
-generate_makefile() {
-  local makefile="xdg_config.mk"
+makefile="xdg_config.mk"
 
-  echo -n > $makefile
+echo -n > $makefile
 
-  find config/** -type d -maxdepth 1 | while read -r dir; do
-    config_name=$(echo "$dir" | sed -r 's/^config\/(.*)$/\1/')
-    config_files=$(find "$dir" -type f | sed -r "s/config\/(.*)/	@\$(LINK_CMD) SRC='config\/\1' DEST='\$(XDG_CONFIG_HOME)\/\1'/g")
-    cat <<EOF >> $makefile
+find config/** -type d -maxdepth 1 | while read -r dir; do
+  config_name=$(echo "$dir" | sed -r 's/^config\/(.*)$/\1/')
+  config_files=$(find "$dir" -type f | sed -r "s/config\/(.*)/	@\$(LINK_CMD) SRC='config\/\1' DEST='\$(XDG_CONFIG_HOME)\/\1'/g")
+  cat <<EOF >> $makefile
 # $config_name
 .PHONY: $config_name
 $config_name: | \$(XDG_CONFIG_HOME)/$config_name
@@ -18,8 +17,4 @@ $config_files
 	@\$(CREATE_TARGET_DIR)
 
 EOF
-  done
-}
-
-# メイン処理
-generate_makefile
+done
